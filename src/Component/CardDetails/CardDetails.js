ee5eb924e540c62849d86b2de9ea81036d4787e9
Mr Surus <mrsurusmk07@gmail.com>
2022-11-10 18:06:05 +0600

    final commit

--- src/Component/CardDetails/CardDetails.js
+++ src/Component/CardDetails/CardDetails.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { FaRegStar } from 'react-icons/fa';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link, useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import useTitle from '../../hooks/useTitle';
 import { AuthContext } from '../Context/AuthProvider/AuthProvider';
 import ReviewSection from './ReviewSection';
@@ -8,19 +10,18 @@ import ReviewSection from './ReviewSection';
 const CardDetails = () => {
     const services = useLoaderData()
     const { title, details, img, _id, price, rating } = services
-    const { user,logOut } = useContext(AuthContext)
+    const { user, } = useContext(AuthContext)
     const [reviewes, setReviewes] = useState([])
-    const [up , setUp] = useState(true)
+    const [up , setUp] = useState(1)
     useTitle('Card-Details')
 
     useEffect(()=>{
         fetch(`https://assignment-eleven-server-five.vercel.app/reviews?service=${_id}`)
         .then(res => res.json())
         .then(data => setReviewes(data))
-    },[up, _id,logOut])
+    },[up])
 
     const handlePlaceReview = (event) => {
-        setUp(!up)
         event.preventDefault();
         const form = event.target
         const email = user?.email || 'unregistered'
@@ -50,6 +51,13 @@ const CardDetails = () => {
         .then(res => res.json())
         .then(data => {
             console.log(data);
+            Swal.fire(
+                'Good job!',
+                'Log In Succesfull!',
+                'success'
+            )
+            
+           setUp(!up)
             form.reset()
         })
 
@@ -62,7 +70,7 @@ const CardDetails = () => {
                     <h2 className="card-title text-3xl">{title}!</h2>
                     <p>{details}</p>
                     <p>Price:  <span className='font-bold text-xl'>{price}$</span></p>
-                    <p>Rating: <span className='font-bold text-xl'>{rating}</span></p>
+                    <p className='flex '>Rating: <span className='font-bold text-xl  ml-3 flex'>{rating}<FaRegStar className='ml-2 mt-1 '></FaRegStar></span></p>
                 </div>
                <PhotoProvider><PhotoView src={img}><img src={img} alt="Soes" /></PhotoView></PhotoProvider> 
             </div>
--- src/Component/Home/Exsection.js
+++ src/Component/Home/Exsection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaRegCheckCircle } from 'react-icons/fa';
+import { FaMedal, FaPenSquare, FaRegCheckCircle, FaSearch } from 'react-icons/fa';
 
 const Exsection = () => {
     return (
@@ -13,6 +13,15 @@ const Exsection = () => {
                         <div className='flex mb-5'><FaRegCheckCircle className='text-3xl text-green-500 mr-3'></FaRegCheckCircle><p className='text-xl font-semibold'>National Geography Photographer</p></div>
                         <div className='flex mb-5'><FaRegCheckCircle className='text-3xl text-green-500 mr-3'></FaRegCheckCircle><p className='text-xl font-semibold'>National Champion </p></div>
                         <div className='flex mb-5'><FaRegCheckCircle className='text-3xl text-green-500 mr-3'></FaRegCheckCircle><p className='text-xl font-semibold'>International 5th </p></div>
+                    </div>
+                </div>
+            </div>
+            <div className="hero min-h-96 bg-base-200 py-10">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold flex mb-16"><FaSearch className='text-blue-400 mr-10'></FaSearch>Watch Review</h1>
+                        <h1 className="text-5xl font-bold flex my-16"><FaPenSquare className='mr-10'></FaPenSquare>Write review </h1>
+                        <h1 className="text-5xl font-bold flex"><FaMedal className='text-yellow-500 mr-10'></FaMedal> Won Price</h1>
                     </div>
                 </div>
             </div>
